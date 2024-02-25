ae0eaf905bf308f55adea006a4166059806b77fd
David <davidsedarous@cmail.cartleon.ca>
2024-02-25 17:12:15 -0500

    Quality of life changes

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
     document.addEventListener("keydown", function(event){
         let idArray = ['1', '2', '3', '4', '5', '6']
         let id = document.activeElement.id;
-        console.log("Checking for event keys: " + event.key);
+        // console.log("Checking for event keys: " + event.key);
         switch(event.key){
             case 'ArrowLeft':          //Left Arrow Key
                 
@@ -14,12 +14,24 @@ document.addEventListener("DOMContentLoaded", function(){
                 }
                 break;
             case 'ArrowUp':          //Up Arrow Key
-                console.log("Moving up:" + id);
-                document.getElementById('5').focus();
+                // console.log("Moving up:" + id);
+                if(document.activeElement.id == "Clear"){
+                
+                    document.getElementById('6').focus();
+                    break;
+                }
+                for (let i = 5; i > 1; i--){
+                    let item = document.getElementById(i.toString())
+                    if (item.value == ''){
+                        item.focus();
+                        break;
+                    }
+                }
+                document.getElementById('1').focus();
                 
                 break;
             case 'ArrowRight':          //Right Arrow Key
-                console.log(id);
+                //console.log(id);
                 if (idArray.includes(id)){
                     id = (parseInt(id)+1).toString();
                     console.log("Moving right:");
@@ -27,7 +39,11 @@ document.addEventListener("DOMContentLoaded", function(){
                 }
                     break;
             case 'ArrowDown':          //Down Arrow Key
-                console.log("Moving down:");
+                //console.log("Moving down:");
+                if(document.activeElement.id == '6'){
+                    document.getElementById("Clear").focus();
+                    break;
+                }
                 document.getElementById('6').focus();
                 break;
             
@@ -37,42 +53,48 @@ document.addEventListener("DOMContentLoaded", function(){
     //Clear Button
     const clearButton = document.getElementById("Clear");
     if(clearButton){
-        console.log("Clear button found");
+        //console.log("Clear button found");
         clearButton.addEventListener('click', clearClick);
+        clearButton.addEventListener("keydown", function(event){
+            if (event.key == "Enter" && document.activeElement.id == "Clear"){
+                clearClick();
+            }
+        })
     }
-    else{
-        console.log(" Clear button not found");
-    }
+    
 
     //Moving on letter input
     const items = document.getElementsByClassName("Letter-Input");
-    console.log("Entering letter-input event listener assigning");
+    //console.log("Entering letter-input event listener assigning");
     for (let i = 0; i < items.length; i++){
-        console.log("i= "+i);
+        //console.log("i= "+i);
         items[i].addEventListener('keydown', function(event){
             if(event.key.length == 1){
+                this.value = event.key.toString();
                 if(parseInt(this.id) < 6){
                     var item = document.getElementById((parseInt(this.id)+1).toString());
                     setTimeout(function(){
                         item.focus();
-                        console.log("Wait-time");
+
+                        //console.log("Wait-time");
                     }, 10);
                     
-                    //item.setSelectionRange(1, 1);
                     
-                    console.log("Moving down the line - debugging");
+                    
+                    //console.log("Moving down the line - debugging");
                 }
-                else{
-                    console.log("Wrong element - didn't move down");
-                }
+                
             }
             if (event.key == 'Backspace'){
-                console.log("backspaced " + parseInt(this.id));
+                //console.log("backspaced " + parseInt(this.id));
                 if(1 < (parseInt(this.id)) && (parseInt(this.id)) < 6 ){
-                    console.log('in backspace');
+                    //console.log('in backspace');
                     this.value = '';
-                    let item = document.getElementById((parseInt(this.id)-1).toString());
-                    item.focus();
+                    var item = document.getElementById((parseInt(this.id)-1).toString());
+                    setTimeout(function(){
+                        item.focus();
+                    }, 10);
+                    
                 }
             }
         })
@@ -83,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 function clearClick(){
-    console.log("clearing");
+    //console.log("clearing");
     
     for(let id = 1; id < 7; id++){
         document.getElementById(id.toString()).value = '';
